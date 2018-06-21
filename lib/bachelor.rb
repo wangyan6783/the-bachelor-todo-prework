563a9fbaac8729c09634faecf720baf603f39c2e
Yan Wang <wangyan6783@gmail.com>
2018-06-21 15:02:23 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -10,7 +10,9 @@ end
 def get_contestant_name(data, occupation)
   data.each { |season, contestants|
     contestants.each { |contestant|
-      if contestant[""]
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
     }
   }
 end
