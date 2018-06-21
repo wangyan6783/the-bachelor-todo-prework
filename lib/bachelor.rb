c7942456f8e80e3ea7ad2528795c4c28418d99af
Yan Wang <wangyan6783@gmail.com>
2018-06-21 16:00:31 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,49 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  season_arr = data[season]
+  season_arr.each { |contestant|
+    if contestant["status"] == "Winner"
+      return contestant["name"].split[0]
+    end
+  }
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each { |season, contestants|
+    contestants.each { |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    }
+  }
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each { |season, contestants|
+    contestants.each { |contestant|
+      if contestant["hometown"] == hometown
+        count += 1
+      end
+    }
+  }
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each { |season, contestants|
+    contestants.each { |contestant|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    }
+  }
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  season_arr = data[season]
+  age_sum = 0
+  season_arr.each { |contestant|
+    age_sum += contestant["age"].to_f
+  }
+  (age_sum / season_arr.length).round
 end
