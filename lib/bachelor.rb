43623bd514fe319b9859dcacf6238204cc5d399d
Yan Wang <wangyan6783@gmail.com>
2018-06-21 15:23:43 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -43,6 +43,6 @@ def get_average_age_for_season(data, season)
   season_arr = data[season]
   age_sum = 0
   season_arr.each { |contestant|
-    age_sum += contestant["age"].to_n
+    age_sum += contestant["age"].to_i
   }
 end
