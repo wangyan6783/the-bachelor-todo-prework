27885349575321d328f3682b6a4448e607a27c5c
Yan Wang <wangyan6783@gmail.com>
2018-06-21 15:00:32 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -8,7 +8,9 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each { |season, |
+    
+  }
 end
 
 def count_contestants_by_hometown(data, hometown)
